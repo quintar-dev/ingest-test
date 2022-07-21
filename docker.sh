c6de3bc1d8d6d172f55b0d824faa875b42e1d834
Vijudev V S <99727626+Viju-DevOps@users.noreply.github.com>
2022-07-21 13:31:42 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,32 +2,67 @@
 
 dock()
 {
-Container_Name="Quintar"
-Image_Name="devops:latest"
-docker ps -aqf name=$Container_Name > id
-Container_ID=$(cat id)
-echo $Container_ID
-echo "funstion dock only"
+    Container_Name="Quintar"
+    Image_Name="quintar/nbadataingest:latest"
+    docker ps -aqf name=$Container_Name > id
+    Container_ID=$(cat id)
+    echo $Container_ID
+    echo "function dock only"
 }
 
 dock_check()
 {
-Container_Name="Quintar"
-Image_Name="devops:latest"
-docker ps -aqf name=$Container_Name > id
-Container_ID=$(cat id)
+    Container_Name="Quintar"
+    Image_Name="quintar/nbadataingest:latest"
+    docker ps -aqf name=$Container_Name > id
+    Container_ID=$(cat id)
+    
+    echo $Container_Name
+    echo $Container_ID
+    docker top $Container_Name
+    
+    if [ $? -eq 0 ]; then
+        echo "Container Exists , Cleaning up ........"
+        docker stop $Container_ID
+        docker rm -vf $Container_ID
+        docker rmi -f $Image_Name
+    else
+        echo "Container Not Exists , Creating ........"
+    fi
+}
 
-echo $Container_Name
-echo $Container_ID
-docker top $Container_Name
+dock_start()
+{
+    Container_Name="Quintar"
+    Image_Name="quintar/nbadataingest:latest"
+    docker ps -aqf name=$Container_Name > id
+    Container_ID=$(cat id)
+    
+    docker build -t $Image_Name .
+    docker run --name $Container_Name -d -p 8080:80 $Image_Name
+    docker ps -a
+}
 
-if [ $? -eq 0 ]; then
-   echo "Container Exists , Cleaning up ........"
-   docker stop $Container_ID
-   docker rm -vf $Container_ID
-   docker rmi -f $Image_Name
-else
-   echo "Container Not Exists , Creating ........"
-fi
+dock_restart()
+{
+    Container_Name="Quintar"
+    Image_Name="quintar/nbadataingest:latest"
+    docker ps -aqf name=$Container_Name > id
+    Container_ID=$(cat id)
+    
+    docker restart $Container_ID
+}
+
+dock_stop()
+{
+    Container_Name="Quintar"
+    Image_Name="quintar/nbadataingest:latest"
+    docker ps -aqf name=$Container_Name > id
+    Container_ID=$(cat id)
+    
+    docker stop $Container_ID
+    docker rm -vf $Container_ID
+    docker rmi -f $Container_Name
+    docker ps -a
 }
 "$@"
